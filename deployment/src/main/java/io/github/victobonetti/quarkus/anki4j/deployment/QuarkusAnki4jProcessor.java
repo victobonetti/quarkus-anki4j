0be7b7ccd2de9d66850da9de7af775450fad093c
vbo <victorbonettixd@gmail.com>
2026-01-26 20:57:13 -0300

    wip

--- deployment/src/main/java/io/github/victobonetti/quarkus/anki4j/deployment/QuarkusAnki4jProcessor.java
+++ deployment/src/main/java/io/github/victobonetti/quarkus/anki4j/deployment/QuarkusAnki4jProcessor.java
@@ -1,7 +1,13 @@
 package io.github.victobonetti.quarkus.anki4j.deployment;
 
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
 
 class QuarkusAnki4jProcessor {
 
@@ -11,4 +17,28 @@ class QuarkusAnki4jProcessor {
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
     }
+
+    @BuildStep
+    void registerPackageForReflection(CombinedIndexBuildItem index,
+            BuildProducer<ReflectiveClassBuildItem> reflection) {
+
+        index.getIndex().getClassesInPackage("com.anki4j.model")
+                .forEach(classInfo -> {
+                    reflection.produce(ReflectiveClassBuildItem.builder(classInfo.name().toString())
+                            .methods(true)
+                            .fields(true)
+                            .build());
+                });
+
+        reflection.produce(ReflectiveClassBuildItem.builder("com.anki4j.Anki4j")
+                .methods(true)
+                .fields(true)
+                .build());
+    }
+
+    @BuildStep
+    RuntimeInitializedClassBuildItem runtimeSqlite() {
+        return new RuntimeInitializedClassBuildItem("org.sqlite.JDBC");
+    }
+
 }
--- deployment/src/test/java/io/github/victobonetti/quarkus/anki4j/test/QuarkusAnki4jDevModeTest.java
+++ deployment/src/test/java/io/github/victobonetti/quarkus/anki4j/test/QuarkusAnki4jDevModeTest.java
@@ -2,7 +2,6 @@ package io.github.victobonetti.quarkus.anki4j.test;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -17,7 +16,6 @@ public class QuarkusAnki4jDevModeTest {
 
     @Test
     public void writeYourOwnDevModeTest() {
-        // Write your dev mode tests here - see the testing extension guide https://quarkus.io/guides/writing-extensions#testing-hot-reload for more information
-        Assertions.assertTrue(true, "Add dev mode assertions to " + getClass().getName());
+        
     }
 }
