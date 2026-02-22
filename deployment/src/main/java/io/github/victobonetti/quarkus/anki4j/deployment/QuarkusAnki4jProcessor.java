143222f38787fd3426fd05fef9c4e1b63462c663
vbo <victorbonettixd@gmail.com>
2026-02-21 23:16:06 -0300

    feat: add .gitignore and update build artifacts and test reports

--- deployment/src/main/java/io/github/victobonetti/quarkus/anki4j/deployment/QuarkusAnki4jProcessor.java
+++ deployment/src/main/java/io/github/victobonetti/quarkus/anki4j/deployment/QuarkusAnki4jProcessor.java
@@ -1,17 +1,14 @@
 package io.github.victobonetti.quarkus.anki4j.deployment;
 
-import io.github.victobonetti.quarkus.anki4j.runtime.Anki4jConfig;
 import io.github.victobonetti.quarkus.anki4j.runtime.Anki4jRecorder;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
-import io.quarkus.deployment.builditem.ConfigMappingBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
 import io.quarkus.deployment.annotations.Record;
-import jakarta.inject.Inject;
 
 class QuarkusAnki4jProcessor {
 
@@ -24,7 +21,7 @@ class QuarkusAnki4jProcessor {
 
     @BuildStep
     void registerPackageForReflection(CombinedIndexBuildItem index,
-                                      BuildProducer<ReflectiveClassBuildItem> reflection) {
+            BuildProducer<ReflectiveClassBuildItem> reflection) {
 
         index.getIndex().getClassesInPackage("com.anki4j.model")
                 .forEach(classInfo -> {
@@ -43,12 +40,6 @@ class QuarkusAnki4jProcessor {
     @BuildStep
     RuntimeInitializedClassBuildItem runtimeSqlite() {
         return new RuntimeInitializedClassBuildItem("org.sqlite.JDBC");
-    }
-
-    @BuildStep
-    ConfigMappingBuildItem registerConfig() {
-        // Isso registra a interface no ecossistema SmallRye Config do Quarkus
-        return new ConfigMappingBuildItem(Anki4jConfig.class, "quarkus.anki4j");
     }
 
     @BuildStep
--- runtime/src/main/java/io/github/victobonetti/quarkus/anki4j/runtime/Anki4jRecorder.java
+++ runtime/src/main/java/io/github/victobonetti/quarkus/anki4j/runtime/Anki4jRecorder.java
@@ -1,16 +1,17 @@
 package io.github.victobonetti.quarkus.anki4j.runtime;
 
 import io.quarkus.runtime.annotations.Recorder;
-import io.smallrye.config.SmallRyeConfig;
 import org.eclipse.microprofile.config.ConfigProvider;
+
+import java.util.Optional;
 
 @Recorder
 public class Anki4jRecorder {
     public void initSystemProperties() {
-        Anki4jConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class)
-                .getConfigMapping(Anki4jConfig.class);
+        Optional<Long> maxPkgSize = ConfigProvider.getConfig()
+                .getOptionalValue("quarkus.anki4j.max-pkg-size-kb", Long.class);
 
-        config.maxPkgSizeKb().ifPresent(value -> {
+        maxPkgSize.ifPresent(value -> {
             System.setProperty("ANKI4J_MAX_PKG_SIZE_KB", String.valueOf(value));
         });
     }
